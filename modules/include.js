5d260cf6531d8628b03f708ed3287dff5d0cb195
ibrahim777764 <ibrahimnaseer896@gmail.com>
2022-03-28 20:28:52 +0500

    updated file

---
+++ modules/include.js
@@ -0,0 +1,11 @@
+export const inputTitle = document.querySelector('.input-title');
+export const inputAuthor = document.querySelector('.input-author');
+export const bookShelf = document.querySelector('.book-shelf');
+export const bookCard = document.createElement('div');
+export const bookForm = document.querySelector('.book-input');
+export const navlist = document.querySelector('#show-list-button');
+export const navadd = document.querySelector('#add-new-button');
+export const navcon = document.querySelector('#contact-button');
+export const list = document.querySelector('#list');
+export const addNew = document.querySelector('#add');
+export const connav = document.querySelector('#contact');
