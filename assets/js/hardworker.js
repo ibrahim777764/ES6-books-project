c123671c8db918a96eb73e8f2d5fea030cf27144
Muhammad Ibrahim Naseer <ibrahimnaseer896@gmail.com>
2022-12-05 21:15:11 +0500

    Update hardworker.js

--- assets/js/hardworker.js
+++ assets/js/hardworker.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
 const removeBtn = document.querySelectorAll('.removeBook');
 const titleInput = document.querySelector('#book_title');
 const authorInput = document.querySelector('#book_author');
